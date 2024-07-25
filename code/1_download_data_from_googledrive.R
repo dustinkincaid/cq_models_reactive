8b652271466412f764cdc161e6bebf6a0180255e
Dustin <dwkincaid@usgs.gov>
2024-07-25 16:48:55 -0500

    Updated file ID for aggregated data file on GD

--- code/1_download_data_from_googledrive.R
+++ code/1_download_data_from_googledrive.R
@@ -4,13 +4,13 @@ if(!require(googledrive)) install.packages("googledrive")
 library(googledrive)
 
 # Shared link to file (with embedded file ID)
-# https://drive.google.com/file/d/1GIU_sFdLHIid868pCUuK02xdcHuwjZYF/view?usp=sharing
+# https://drive.google.com/file/d/1aJcTg1f_9ks7oyxUItZBa9iHXss3eUob/view?usp=sharing
 
 # Put googledrive into a de-authorized state
 drive_deauth()
 
 # Pull embedded file ID from 
-public_file <-  drive_get(as_id("1GIU_sFdLHIid868pCUuK02xdcHuwjZYF"))
+public_file <-  drive_get(as_id("1aJcTg1f_9ks7oyxUItZBa9iHXss3eUob"))
 drive_download(file = public_file, 
                path = "data/nh_cq_data_filtered.csv",
                overwrite = TRUE)
